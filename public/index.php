aa7d49eb0e1f4cdbf1fbb2f98d496183969171b4
stanley <xinhua.stanley@gmail.com>
2020-06-15 10:19:16 +0800

    hello world

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Date: 2020/6/15 10:03
+ */
+require __DIR__.'/../src/app.php';
+ $app = new bosomFriend\App();
+ echo $app->index();
---
+++ src/app.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Date: 2020/6/15 09:50
+ */
+namespace bosomFriend;
+
+class App{
+    public function index(){
+        return 'you ain\'t got any friends'.PHP_EOL;
+    }
+}
